022c24228a8a08e288206cd4cdabc21649803f07
goooooonight <fyf200436@gmail.com>
2025-07-26 23:18:14 +0800

    style:优化了组件文件夹的构成

--- src/views/home/RbHome.vue
+++ src/views/home/RbHome.vue
@@ -1,10 +1,12 @@
 <script setup>
-import HomeBanner from '@/components/HomeBanner.vue'
+import HomeBanner from '@/components/home/HomeBanner.vue'
+import HomeNew from '@/components/home/HomeNew.vue'
 </script>
 
 <template>
-  <div class="h-[600px]">
+  <div class="h-[1000px]">
     <HomeBanner></HomeBanner>
+    <HomeNew></HomeNew>
   </div>
 </template>
 
--- src/views/layout/RbLayout.vue
+++ src/views/layout/RbLayout.vue
@@ -1,8 +1,8 @@
 <script setup>
-import LayoutNav from '@/components/LayoutNav.vue'
-import LayoutHeader from '@/components/LayoutHeader.vue'
-import LayoutFooter from '@/components/LayoutFooter.vue'
-import LayoutFixedNav from '@/components/LayoutFixedNav.vue'
+import LayoutNav from '@/components/layout/LayoutNav.vue'
+import LayoutHeader from '@/components/layout/LayoutHeader.vue'
+import LayoutFooter from '@/components/layout/LayoutFooter.vue'
+import LayoutFixedNav from '@/components/layout/LayoutFixedNav.vue'
 import { useCateStore } from '@/stores/modules/category'
 
 // 分类模块对象
