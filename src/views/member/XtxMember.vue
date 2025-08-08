6bfff169acecac01f75d1e71044578a1c07c6291
goooooonight <fyf200436@gmail.com>
2025-08-08 23:49:38 +0800

    feat:在个人中心里新增了菜单所选项根据路由路径进行变化的功能

--- src/views/member/XtxMember.vue
+++ src/views/member/XtxMember.vue
@@ -1,22 +1,30 @@
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+// 获取路由对象
+const route = useRoute()
+// 获取路径（响应式）
+const path = computed(() => route.path)
+</script>
 
 <template>
   <div class="xtx-container">
     <div class="mt-6 flex">
       <!-- 左侧菜单栏 -->
       <div class="w-[230px]">
-        <el-menu default-active="1-1" router>
+        <el-menu :default-active="path" router>
           <el-sub-menu index="1">
             <template #title>
               <span>我的账户</span>
             </template>
             <el-menu-item-group>
               <el-menu-item index="/member/user">个人中心</el-menu-item>
-              <el-menu-item index="1-2">消息通知</el-menu-item>
-              <el-menu-item index="1-3">个人信息</el-menu-item>
-              <el-menu-item index="1-4">安全设置</el-menu-item>
-              <el-menu-item index="1-5">我的足迹</el-menu-item>
-              <el-menu-item index="1-6">地址管理</el-menu-item>
+              <el-menu-item disabled index="#">消息通知</el-menu-item>
+              <el-menu-item disabled index="1-3">个人信息</el-menu-item>
+              <el-menu-item disabled index="1-4">安全设置</el-menu-item>
+              <el-menu-item disabled index="1-5">我的足迹</el-menu-item>
+              <el-menu-item disabled index="1-6">地址管理</el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
 
@@ -26,9 +34,9 @@
             </template>
             <el-menu-item-group>
               <el-menu-item index="/member/order">我的订单</el-menu-item>
-              <el-menu-item index="2-2">优惠券</el-menu-item>
-              <el-menu-item index="2-3">评价晒单</el-menu-item>
-              <el-menu-item index="2-4">售后服务</el-menu-item>
+              <el-menu-item disabled index="2-2">优惠券</el-menu-item>
+              <el-menu-item disabled index="2-3">评价晒单</el-menu-item>
+              <el-menu-item disabled index="2-4">售后服务</el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
 
@@ -37,9 +45,9 @@
               <span>我的收藏</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item index="3-1">收藏的商品</el-menu-item>
-              <el-menu-item index="3-2">收藏的专题</el-menu-item>
-              <el-menu-item index="3-3">关注的品牌</el-menu-item>
+              <el-menu-item disabled index="3-1">收藏的商品</el-menu-item>
+              <el-menu-item disabled index="3-2">收藏的专题</el-menu-item>
+              <el-menu-item disabled index="3-3">关注的品牌</el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
 
@@ -48,8 +56,8 @@
               <span>帮助中心</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item index="4-1">帮助中心</el-menu-item>
-              <el-menu-item index="4-2">在线客服</el-menu-item>
+              <el-menu-item disabled index="4-1">帮助中心</el-menu-item>
+              <el-menu-item disabled index="4-2">在线客服</el-menu-item>
             </el-menu-item-group>
           </el-sub-menu>
         </el-menu>
